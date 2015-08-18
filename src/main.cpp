35243524bf21b564912e5818f042d6d7b155b7df
Stefan Petscharnig <stepetsc@gmail.com>
2015-08-18 11:32:20 +0200

    added dashready support

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,7 @@
  *	./vqtool -p 500.mp4 -P mp4 -r 1000.mp4 -R mp4 --ssim --psnr --vqm --verbose 1 --log 1 -t 1,2,4
  *	./vqtool -p processed.mp4 -P mp4 -r reference.mp4 -R mp4 --ssim --psnr --verbose 1 --log 1 -t 1,2,4,6,8
  *  ./vqtool -p processed.mp4 -P mp4 -r reference.mp4 -R mp4 --vqm --verbose 1 --log 3 -t 1,2,4,6,8
+ *	./vqtool -p processed.mp4 -P mp4 -r reference.mp4 -R mp4 --ssim --psnr --verbose 1 --log 3 -t 1,2,4,6,8 --dashready
  */
 
 
@@ -47,6 +48,7 @@ int main(int argc, char **argv){
 	int psnr_flag = 0;
 	int ssim_flag = 0;
 	int vqm_flag = 0;
+	int dashready_flag = 0;
 
 	int verbose = 0;
 	int loglvl = 0;
@@ -58,6 +60,9 @@ int main(int argc, char **argv){
 			  {"psnr", no_argument, &psnr_flag, 1},
 			  {"ssim", no_argument, &ssim_flag, 1},
 			  {"vqm",  no_argument, &vqm_flag , 1},
+
+			  {"dashready",  no_argument, &dashready_flag , 1},
+
  			 /* These options don’t set a flag.
 				 We distinguish them by their indices. */
 			  {"help",  			no_argument, 		0, 'h'},
@@ -284,77 +289,123 @@ int main(int argc, char **argv){
 		timesString="2";
 	}
 
-	std::vector<double> res;	
+	
 	vector<string> time_vector = split(timesString, ',');
 	
 	//1 slice hat 0.2 sekunden
 	
 	std::ofstream resfile;
+	std::ofstream mpd_file;
+
+	std::vector<double> psnr_res;
+	std::vector<double> ssim_res;
+	std::vector<double> vqm_res;
 
 	int time;
 	int slices;
 	std::string filename;
+	std::string mpd_filename;
+
+	int size;
+
 	for(uint i=0; i<time_vector.size(); i++){
 		
 		time = atoi(time_vector[i].c_str());
 		slices = time * rR->getFps()/ framesPerSlice;
 
 		if(psnr_flag){
-			res.clear();
+			psnr_res.clear();
 
 			dbg("psnr time collapse", verbose);
 			psnr.timeCollapse(slices);
 			dbg("psnr get metric value", verbose);
-			psnr.getMetricValue(&res);	
+			psnr.getMetricValue(&psnr_res);	
 			dbg("psnr write results", verbose);		
 
 			filename = out_prefix + "_psnr_" + time_vector[i] + "s.csv";
 			remove( filename.c_str() ); //remove file if exists
 			resfile.open (filename.c_str(), std::ios::out | std::ios::app); 
-			for(std::vector<int>::size_type i = 0; i < res.size(); i+=1) {
-				resfile << i << ";" << res[i] << std::endl;
-				dbg(res[i], verbose);
+			for(std::vector<int>::size_type i = 0; i < psnr_res.size(); i+=1) {
+				resfile << i << ";" << psnr_res[i] << std::endl;
+				dbg(psnr_res[i], verbose);
 			}	
 			resfile.close();
+			size = psnr_res.size();
 		}
 		if(ssim_flag){
-			res.clear();
+			ssim_res.clear();
 
 			dbg("ssim time collapse", verbose);
 			ssim.timeCollapse(slices);
 			dbg("ssim get metric value", verbose);
-			ssim.getMetricValue(&res);
+			ssim.getMetricValue(&ssim_res);
 			dbg("ssim write results", verbose);	
 	
 			filename = out_prefix + "_ssim_" + time_vector[i] + "s.csv";
 			remove( filename.c_str() ); //remove file if exists
 			resfile.open (filename.c_str(), std::ios::out | std::ios::app);
-			for(std::vector<int>::size_type i = 0; i < res.size(); i+=1) {
-				resfile << i << ";" << res[i] << std::endl;
-				dbg(res[i], verbose);
+			for(std::vector<int>::size_type i = 0; i < ssim_res.size(); i+=1) {
+				resfile << i << ";" << ssim_res[i] << std::endl;
+				dbg(ssim_res[i], verbose);
 			}
 			resfile.close();
+			size = ssim_res.size();
 		}
 		if(vqm_flag){
-			res.clear();
+			vqm_res.clear();
 
 			dbg("vqm time collapse", verbose);
 			vqm.timeCollapse(slices);
 			dbg("vqm get metric value", verbose);
-			vqm.getMetricValue(&res);
+			vqm.getMetricValue(&vqm_res);
 			dbg("vqm write results", verbose);	
 
 			filename = out_prefix + "_vqm_" + time_vector[i] + "s.csv";
 			remove( filename.c_str() ); //remove file if exists
 			resfile.open (filename.c_str(), std::ios::out | std::ios::app); 
 
-			for(std::vector<int>::size_type i = 0; i < res.size(); i+=1) {
-				resfile << i << ";" << res[i] << std::endl;
-				std::cout << res[i];
+			for(std::vector<int>::size_type i = 0; i < vqm_res.size(); i+=1) {
+				resfile << i << ";" << vqm_res[i] << std::endl;
+				std::cout << vqm_res[i];
 			}	
-			resfile.close();	
+			resfile.close();
+			size = vqm_res.size();	
+		}
+
+		if(dashready_flag){
+			/* print mpd file extension to file */
+			mpd_filename = out_prefix + "_mpdExtension_" + time_vector[i] + "s.csv";
+			remove( mpd_filename.c_str() ); //remove file if exists
+			mpd_file.open (mpd_filename.c_str(), std::ios::out | std::ios::app); 
+
+			mpd_file << "<Quality>" << std::endl;
+			for(int i = 0; i < size ; i+=1) {
+
+				mpd_file << "\t<SegInfo SegNr=\"" << (i+1) << "\" QoS=\"";
+				if(psnr_flag) 
+					mpd_file << "(PSNR," << psnr_res[i] << ")";
+
+				if(psnr_flag && (ssim_flag || vqm_flag)) 
+					mpd_file << ";";
+
+				if(ssim_flag) 
+					mpd_file << "(SSIM," << ssim_res[i] << ")";
+
+				if(ssim_flag && vqm_flag) 
+					mpd_file << ";";
+				if(vqm_flag)  
+					mpd_file << "(VQM,"  << vqm_res[i] << ")";
+
+				mpd_file << "\"";
+				mpd_file << "/>";
+				mpd_file << std::endl;
+			}
+			mpd_file << "</Quality>" << std::endl;
+			mpd_file.close();
 		}
 	}
+
+	
 
 	dbg("shutting down...", verbose);
 	
@@ -402,6 +453,8 @@ void printUsage(){
  std::cout << "--psnr\n\tcalculate peak signal to noise ratio" << std::endl;
  std::cout << "--ssim\n\tcalculate structured similarity" << std::endl;
  std::cout << "--vqm\n\tcalculate vqm" << std::endl;
+
+ std::cout << "--dashready\n\tproduce dashready output to insert into mpds" << std::endl;
 
  std::cout << "-t, --time <t> \n\ttemporally collapse in time" << std::endl;
  std::cout << "\tt is a comma separated list with segment length in seconds, e.g. 1,2,4 " << std::endl;
